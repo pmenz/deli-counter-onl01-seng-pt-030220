505e41c64d5b84f71e37f30ab9bd0dd43206198e
Pablo Menz <p.menz.r@gmail.com>
2020-03-12 01:15:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,12 @@ def line(katz_deli)
     puts "The line is currently empty."
     return
     else
-     str="the line is currently"
+     str="The line is currently: "
      katz_deli.each.with_index(1) do|person,i|
        str << " #{i}. #{person}"
      end
    end
-
+str
 end
 
 def take_a_number(katz_deli, new_customer)
